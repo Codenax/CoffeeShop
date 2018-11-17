6cb62ae88cbfc1287e3a0a3dc89b42eb8adf9cb4
Codenax <44666712+Codenax@users.noreply.github.com>
2018-11-17 20:57:40 +0600

    One order

---
+++ CoffeeOderWindowsFormsApplication - one Order/CoffeeOderWindowsFormsApplication/CoffeeOrderUl.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeeOderWindowsFormsApplication
+{
+    public partial class CoffeeOrderUl : Form
+    {
+        public CoffeeOrderUl()
+        {
+            InitializeComponent();
+        }
+
+
+        List<string> customarNameList = new List<string>();
+        List<string> uniteList = new List<string>();
+        private void oderButton_Click(object sender, EventArgs e)
+        {
+            string cusName = coffeeNamecomboBox.Text;
+            customarNameList.Add(cusName);
+            string unite_L= unitTextBox.Text;
+            uniteList.Add(unite_L);
+        }
+            
+       
+        private void buyButton_Click(object sender, EventArgs e)
+        {
+
+            string result = "Name";
+            int unite = 0;
+           
+            
+            foreach (string cusName in customarNameList)
+            {
+                result += cusName + "\t" + uniteList[unite] + "\n";
+                unite++;
+            }
+            receptRichTextBox.Text = result;                   
+         }
+
+    }
+}
